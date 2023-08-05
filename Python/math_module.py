709befb53b315db4d6ccbc78bc921dd77c7cf3dd
Sabarinath K <sabarinathodc@gmai.com>
2023-08-05 20:27:58 +0530

    updated for modules

---
+++ Python/math_module.py
@@ -0,0 +1,18 @@
+def sub(b,a=0):   # here 0 is the default value of the function          
+    '''
+    This function is for subtracting two number
+    
+    arguments:
+       a = default=0,int
+       b = int
+    
+    example: 
+    >>> sum (a=3,b=1)
+        c=3-1
+    ... a,b,c=3,1,2
+    
+    
+    
+    '''
+    c=a-b
+    return a,b,c
